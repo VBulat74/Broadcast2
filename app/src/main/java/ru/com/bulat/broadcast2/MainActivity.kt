b26bb06814322bc0fd9008a6b786497ac3987328
VBulat74 <Vladimir.Bulat@gmail.com>
2024-03-02 17:52:01 +0300

    Lesson 05 (LocalBroadcastManager)

--- app/src/main/java/ru/com/bulat/broadcast2/MainActivity.kt
+++ app/src/main/java/ru/com/bulat/broadcast2/MainActivity.kt
@@ -1,11 +1,41 @@
 package ru.com.bulat.broadcast2
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    private lateinit var progressBar : ProgressBar
+
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(p0: Context?, p1: Intent?) {
+            if (p1?.action == "loaded") {
+                val percent : Int = p1.getIntExtra("percent", 0)
+                progressBar.progress = percent
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        progressBar = findViewById(R.id.progressBar)
+
+        val intenFilter = IntentFilter().apply {
+            addAction("loaded")
+        }
+        registerReceiver(receiver, intenFilter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
     }
 }
